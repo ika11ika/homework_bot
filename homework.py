58a420ef64eb39280c75726506596d93be9cb637
Ирина Московкина <ika11ika@yandex.ru>
2022-10-01 16:07:40 +0300

    Fixed review notes

--- homework.py
+++ homework.py
@@ -1,6 +1,8 @@
 import logging
 import os
+import sys
 import time
+from http import HTTPStatus
 
 import requests
 import telegram
@@ -19,17 +21,11 @@ ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
-
-logging.basicConfig(
-    filename='main.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-)
 
 
 def send_message(bot, message):
@@ -50,7 +46,7 @@ def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    if homework_statuses.status_code != 200:
+    if homework_statuses.status_code != HTTPStatus.OK:
         assert False
         logging.error(f'Ошибка при запросе {homework_statuses.status_code}')
     return homework_statuses.json()
@@ -65,11 +61,11 @@ def check_response(response):
     if not homeworks:
         logging.error('В ответе API нет списка работ')
         raise exceptions.APIResponseException()
-    if homeworks == []:
-        logging.error('Работ не найдено')
-        raise exceptions.APIResponseException()
     if type(homeworks) != list:
         logging.error('В ответе API работы представлены не списком')
+        raise exceptions.APIResponseException()
+    if homeworks == []:
+        logging.error('Работ не найдено')
         raise exceptions.APIResponseException()
     return homeworks
 
@@ -78,22 +74,20 @@ def parse_status(homework):
     """Обработка статуса полученной домашки."""
     homework_name = homework['homework_name']
     homework_status = homework['status']
-    verdict = HOMEWORK_STATUSES[homework_status]
+    verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Проверка, что токены найдены."""
-    if None in (PRACTICUM_TOKEN, TELEGRAM_TOKEN):
-        logging.critical('Отсутствие обязательных переменных окружения')
-        return False
-    return True
+    return all(PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID)
 
 
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
-        return
+        sys.exit("Не удалось загрузить данные токенов.")
+
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     response = get_api_answer(current_timestamp)
@@ -110,10 +104,16 @@ def main():
                 status = message
             time.sleep(RETRY_TIME)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            print(message)
+            logging.error(f'Сбой в работе программы: {error}')
             time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
+
+    logging.basicConfig(
+        filename='main.log',
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    )
+
     main()
