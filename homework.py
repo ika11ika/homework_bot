fbd7f5012baf317837072ff3b9a29f14dae5c6ec
Ирина Московкина <ika11ika@yandex.ru>
2022-10-03 18:39:37 +0300

    Remove time params to check api answer

--- homework.py
+++ homework.py
@@ -44,7 +44,7 @@ def send_message(bot, message):
 def get_api_answer(current_timestamp):
     """Запрос ответа от API."""
     timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
+    params = {'from_date': 0}
     try:
         homework_statuses = requests.get(
             ENDPOINT,
