59b176de4b5de0e44536322b15961fd72b3a7841
Ирина Московкина <ika11ika@yandex.ru>
2022-10-03 18:42:00 +0300

    Remove time params to check api answer

--- homework.py
+++ homework.py
@@ -110,9 +110,9 @@ def main():
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
             message = parse_status(homeworks[0])
-            if (message != status):
-                send_message(bot, message)
-                status = message
+            #if (message != status):
+            send_message(bot, message)
+            status = message
             time.sleep(RETRY_TIME)
     except Exception as error:
         logging.error(error.__str__)
